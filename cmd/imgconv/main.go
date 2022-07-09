ab07a63a5f458e6adf0b8335070967f0d26ee3da
kaz-i-54 <torune.summer@gmail.com>
2022-07-09 14:26:59 +0900

    moved to step01

---
+++ cmd/imgconv/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+  imgconv "github.com/edge2992/imgconv"
+)
+
+var (
+	flagTo   = imgconv.PNG
+	flagFrom = imgconv.JPEG
+)
+
+func init() {
+	flag.Var(&flagTo, "to", "after format")
+	flag.Var(&flagFrom, "from", "before format")
+}
+
+func main() {
+	if err := imgconv.ConvertAll(os.Args[1], flagFrom, flagTo); err != nil {
+		fmt.Fprintln(os.Stderr, "エラー:", err)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/edge2992/imgconv
+
+go 1.18
